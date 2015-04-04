17f1de6f0c94e33d10adeda89a636f6cfcf88978
OriginalSin <saleks@scanex.ru>
2015-04-04 11:27:54 +0300

    addRosreestrMixin

--- src/TileLayer.Rosreestr.js
+++ src/TileLayer.Rosreestr.js
@@ -1,30 +1,36 @@
 /*
  * L.TileLayer.Rosreestr
  */
++function() {
+var addRosreestrMixin = function(BaseClass) {
+    return BaseClass.extend({
+        options: {
+            tileSize: 1024
+        },
 
-L.TileLayer.Rosreestr = L.TileLayer.extend({
-    options: {
-        tileSize: 1024
-    },
+        getTileUrl: function (tilePoint) {
+            var map = this._map,
+                crs = map.options.crs,
+                tileSize = this.options.tileSize,
+                nwPoint = tilePoint.multiplyBy(tileSize),
+                sePoint = nwPoint.add([tileSize, tileSize]);
+     
+            var nw = crs.project(map.unproject(nwPoint, tilePoint.z)),
+                se = crs.project(map.unproject(sePoint, tilePoint.z)),
+                bbox = [nw.x, se.y, se.x, nw.y].join(',');
 
-    getTileUrl: function (tilePoint) {
-        var map = this._map,
-            crs = map.options.crs,
-            tileSize = this.options.tileSize,
-            nwPoint = tilePoint.multiplyBy(tileSize),
-            sePoint = nwPoint.add([tileSize, tileSize]);
- 
-        var nw = crs.project(map.unproject(nwPoint, tilePoint.z)),
-            se = crs.project(map.unproject(sePoint, tilePoint.z)),
-            bbox = [nw.x, se.y, se.x, nw.y].join(',');
+            return L.Util.template(this._url, L.extend({
+                s: this._getSubdomain(tilePoint),
+                bbox: bbox
+            }, this.options));
+        }
+    })
+};
 
-        return L.Util.template(this._url, L.extend({
-            s: this._getSubdomain(tilePoint),
-            bbox: bbox
-        }, this.options));
-    }
-});
+L.TileLayer.Rosreestr = addRosreestrMixin(L.TileLayer);
 
 L.tileLayer.Rosreestr = function (url, options) {
     return new L.TileLayer.Rosreestr(url, options);
 };
+
+}();
